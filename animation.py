802666d474033a25bc369c09d1bb6f28ddd94128
ARBYZ148852228 <anonim15255@gmail.com>
2025-01-25 14:09:56 +0300

    123
    123

--- animation.py
+++ animation.py
@@ -1,13 +1,5 @@
 import tkinter as tk
 
-# Загрузка изображений
-bird_images = [
-    tk.PhotoImage(file="bird1.png"),
-    tk.PhotoImage(file="bird2.png"),
-    tk.PhotoImage(file="bird3.png")
-]
-
-
 def animate_bird():
     global current_image_index
 
@@ -21,19 +13,27 @@ def animate_bird():
     current_image_index = (current_image_index + 1) % len(bird_images)
 
     # Запускаем следующую итерацию анимации через 100 мс
-    root.after(100, animate_bird)
+    root.after(300, animate_bird)
 
 
 # Создаем главное окно приложения
 root = tk.Tk()
 root.title("Анимация полета птицы")
 
+
+# Загрузка изображений
+bird_images = [
+    tk.PhotoImage(file="bird1.png"),
+    tk.PhotoImage(file="bird2.png"),
+    tk.PhotoImage(file="bird3.png")
+]
+
 # Создаем канвас для отображения анимации
-canvas = tk.Canvas(root, width=500, height=300)
+canvas = tk.Canvas(root, width=1000, height=500)
 canvas.pack()
 
 # Рисуем начальное положение птицы
-x, y = 50, 150
+x, y = 0, 0
 bird = canvas.create_image(x, y, anchor='nw', image=bird_images[0])
 
 # Индекс текущего изображения
